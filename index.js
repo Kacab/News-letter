58eed584b40d557c176d224b5c9dc15688acdc47
unknown <kacabibrahim@gmail.com>
2024-12-19 10:20:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const but =  document.querySelector("#But")
+const main = document.querySelector('.main')
 
+const but2 =  document.querySelector('#But2')
+const thanks = document.querySelector('.thanks')
+
+
+but2.addEventListener('click',function(){
+    thanks.remove()
+    window.location.href = 'index.html'
+})
 but.addEventListener('click',function(){
+    thanks.style.display = 'block' 
+    thanks.style.display = 'flex'
+    main.remove()
    
+
 })
